405f89a317f37efde1611fe9650260621b65c1cf
Dennis <dennis.mok@live.com>
2020-09-16 12:57:55 +1000

    a

--- Week0/bmi/bmi.py
+++ Week0/bmi/bmi.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 
+
 def calc_bmi(weight, height):
     return float(weight / (height ** 2))
 
@@ -19,6 +20,14 @@ def main():
         with open("Summary.txt", "a") as f:
             f.writelines(str(bmi) + "\n")
         bmi = read_input()
+
+    vals = np.array([])  
+    with open("Summary.txt", "r") as f:
+        for line in f.readlines():
+            vals = np.append(vals, [float(line)])
+    mean = np.mean(vals)
+    stddev = np.std(vals)
+    print(f"mean={mean:.2f} stddev={stddev:.2f}")
 
 if __name__ == "__main__":
     main()
