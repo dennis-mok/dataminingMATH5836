b2aec576d443e49efaa89b4dd88fb03cd80cabd5
Dennis <dennis.mok@live.com>
2020-09-19 15:52:34 +1000

    a
    Signed-off-by: Dennis <dennis.mok@live.com>

---
+++ Week0/bmi/bmi_class.py
@@ -0,0 +1,18 @@
+from bmi import calc_bmi
+
+class BMI:
+    def __init__(self, weight, height):
+        self.weight = weight
+        self.height = height
+    
+    self.bmi = calc_bmi(self.weight, self.height)
+    
+    def get_bmi():
+        return self.bmi
+    
+    
+def main():
+
+
+if __name__ == "__main__":
+    main()
