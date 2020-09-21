78cd4c404be2f9f7f4e44171094e8ac5f56fa450
Dennis <dennis.mok@live.com>
2020-09-21 22:03:55 +1000

    first commit

---
+++ Week1/iris/process.py
@@ -0,0 +1,18 @@
+# Use simple find and replace to convert the class labels to 1, 2, and 3 in the dataset. 
+
+# Read the data and report mean and standard deviation for each column in the features (4 features)
+
+# Report the class distribution (i. e number of instances for each class)
+
+# Show histogram for each feature. Note you need to use a single function/method that outputs the histogram with a given filename. eg. feature1.png which is given as a parameter to the function. A for loop should be used to call the function/method
+
+# Split data into a train and test test. Use 60 percent data in the training and test set which is assigned i. randomly ii. assigned by first 60 percent as train and rest as test. 
+
+# Use previous functions to report the mean and standard deviation of the train and test set and class distribution and also the histograms for each feature. 
+
+# Create another subset of the train and test set where only 1 feature selected by the user makes the dataset with the class. 
+
+# Create a subset of the dataset where you consider only instances that feature class 1 or 2, so that you treat this problem as a binary classification problem later, i.e save it as binary_iristrain.txt and binary_iristest.txt. Carry out the stats and visuals in Step 6 for this dataset. 
+
+# Can you normalise the input features between [0 and 1] ? Write code that can do so and save normalised versions.
+
